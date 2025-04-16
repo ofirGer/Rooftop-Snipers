c77341925ec27aceff6c5c083056054ebe240e9b
ofirGer <142690409+ofirGer@users.noreply.github.com>
2025-04-16 17:31:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygame
-import player
+import pygame, player, gun
+
 # Initialize Pygame
 pygame.init()
 
@@ -19,14 +19,12 @@ ROOF_X = (WIDTH - ROOF_WIDTH) // 2
 ROOF_Y = HEIGHT - ROOF_HEIGHT  # Position the roof at the bottom
 
 
-
-
 def main():
     """Main game loop."""
 
     clock = pygame.time.Clock()
     player1 = player.Player(ROOF_X + 20, ROOF_Y)  # Player starts on the left side of the roof
-
+    player_gun = gun.Gun(player1, screen)
     running = True
     while running:
         clock.tick(60)  # 60 FPS
@@ -38,15 +36,17 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
                 player1.jump()
 
+        # Drawing
+        screen.fill(BACKGROUND_COLOR)  # Sky
+        pygame.draw.rect(screen, ROOF_COLOR, (ROOF_X, ROOF_Y, ROOF_WIDTH, ROOF_HEIGHT))  # Draw the roof
         # Game logic
         player1.apply_gravity()
         player1.lean_and_move()  # Leaning movement
         player1.update_position()  # Apply smooth horizontal movement
-
-        # Drawing
-        screen.fill(BACKGROUND_COLOR)  # Sky
-        pygame.draw.rect(screen, ROOF_COLOR, (ROOF_X, ROOF_Y, ROOF_WIDTH, ROOF_HEIGHT))  # Draw the roof
+        player_gun.update_position()
+        player_gun.shoot()
         player1.draw(screen)
+        player_gun.draw()
         pygame.display.flip()
 
     pygame.quit()
